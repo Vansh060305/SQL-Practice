8bacbd85eec61d5c274464830c069e786052aa87
Vansh060305 <vr3275434@gmail.com>
2026-08-09 22:21:46 +0530

    Add Weather Observation station 15 solution

---
+++ 03_Aggregation/10_Weather_Observation_15.sql
@@ -0,0 +1,13 @@
+--- Plateform: HackerRank
+-- Difficulty: Easy
+-- Topic: Aggregation
+
+
+
+select round(max(LONG_W),4)
+from STATION 
+where LAT_N =(
+    select max(LAT_N)
+    from station 
+    where LAT_N <137.2345
+);
